699efabbcce402a60d3c2d37d720252ff52917a7
0xp0tato <ninadmaster05@gmail.com>
2023-03-28 23:37:43 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+import "./App.css"
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar/>
+      <div className='intro'>
+        HI MY NAME IS NINAD SAJWAN
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='logo'>logo</div>
+        <div className="nav-items">
+          <div>Home</div>
+          <div>Home</div>
+          <div>Home</div>
+          <div>Home</div>
+          <div>Home</div>
+        </div>
+      </div>
+  )
+}
+
+export default Navbar
