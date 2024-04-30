292ff919527cc03866bfa4e7d343a82f3889b8d1
0xp0tato <ninadmaster05@gmail.com>
2024-04-30 12:47:45 +0530

    UPDATE : current company name

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,6 @@ import myAvatar from "../assets/8bitpix.png";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 export default function Hero() {
-
   return (
     <div
       className="bg-gray-100 min-h-screen flex flex-col justify-center items-center p-4 lg:p-8"
@@ -12,9 +11,13 @@ export default function Hero() {
       <div className="w-full md:w-[80%] lg:w-[60%] mx-auto bg-white rounded-lg shadow-xl flex flex-col md:flex-row items-center p-6 md:p-12">
         <div className="md:w-1/2 mb-4 md:mb-0">
           <p className="text-lg md:text-xl text-gray-500">$ whoami</p>
-          <p className="text-xl md:text-3xl font-bold">{">"} Hi, I'm Ninad Sajwan.</p>
+          <p className="text-xl md:text-3xl font-bold">
+            {">"} Hi, I'm Ninad Sajwan.
+          </p>
           <p className="text-sm md:text-lg text-gray-600 mb-4">
-            {">"} A Passionate Software Engineer. SSE @ Infosys
+            {">"} A Passionate Developer
+            <br />
+            {">"} Software Engineer @ Squareboat
           </p>
           <div className="flex gap-2 md:gap-3">
             <a
@@ -27,8 +30,17 @@ export default function Hero() {
                 className="text-blue-500 text-xl md:text-2xl hover:text-blue-700 transition duration-300 ease-in-out"
               />
             </a>
-            <a href="https://leetcode.com/0xp0tat0/" target="_blank" rel="noopener noreferrer">
-              <img width="30" height="30" src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/external-level-up-your-coding-skills-and-quickly-land-a-job-logo-color-tal-revivo.png" alt="external-level-up-your-coding-skills-and-quickly-land-a-job-logo-color-tal-revivo"/>
+            <a
+              href="https://leetcode.com/0xp0tat0/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                width="30"
+                height="30"
+                src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/external-level-up-your-coding-skills-and-quickly-land-a-job-logo-color-tal-revivo.png"
+                alt="external-level-up-your-coding-skills-and-quickly-land-a-job-logo-color-tal-revivo"
+              />
             </a>
             <a
               href="https://github.com/0xp0tato"
@@ -56,7 +68,9 @@ export default function Hero() {
         </div>
       </div>
       <div className="max-w-full md:max-w-7xl mx-auto mt-6 lg:mt-8">
-        <p className="text-lg md:text-xl font-semibold text-center">Tech Stack</p>
+        <p className="text-lg md:text-xl font-semibold text-center">
+          Tech Stack
+        </p>
         <div className="flex gap-2 md:gap-3 justify-center mt-3">
           <img
             src="https://skillicons.dev/icons?i=html,css,javascript,typescript,c,cpp,python,next,nodejs,express,firebase,tailwind,react,redux,mongodb,mysql"
